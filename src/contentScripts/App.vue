eb464a9b8946c1eb0d93d2c6c4958b6b8930bfbd
Oliver Manzi <olivermanzi.dev@gmail.com>
2023-03-20 22:07:34 +0100

    [0.10.2] patch: removes content-script btn

--- src/contentScripts/App.vue
+++ src/contentScripts/App.vue
@@ -1,28 +1,9 @@
 <script setup lang="ts">
-import { useToggle } from '@vueuse/core'
 import 'uno.css'
-
-const [ show, toggle ] = useToggle(false)
 </script>
 
 <template>
-  <div class="fixed right-0 bottom-15 m-5 z-100 flex items-end font-sans select-none leading-1em">
-    <div
-      class="bg-white text-gray-800 rounded-lg shadow w-max h-min"
-      p="x-4 y-2"
-      m="y-auto r-2"
-      transition="opacity duration-300"
-      :class="show ? 'opacity-100' : 'opacity-0'">
-      <h1 class="text-lg">
-        Vitesse WebExt
-      </h1>
-      <p class="mt-2 opacity-50">This is the {{ $app.context }} page</p>
-    </div>
-    <div
-      class="flex w-10 h-10 rounded-full shadow cursor-pointer"
-      bg="teal-600 hover:teal-700"
-      @click="toggle()">
-      <pixelarticons-power class="block m-auto text-white text-lg" />
-    </div>
+  <div>
+    <!-- insert content screen ui here -->
   </div>
 </template>
