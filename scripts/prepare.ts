68feeeb1369326624b8e6f3895b633e9d9e5e766
Oliver Manzi <olivermanzi.dev@gmail.com>
2023-04-18 18:02:25 +0200

    fix: ext components were not building properly in development (v0.23.4)

--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -8,21 +8,22 @@ import { isDev, log, port, r, isManifest3 } from './utils'
  * Stub index.html to use Vite in development
  */
 async function stubIndexHtml() {
-  const views = [
+  let views = [
     'options',
-    'popup'
+    'popup',
+    'background'
   ]
 
   // manifest v3 does not support background pages
-  if (!isManifest3) {
-    views.push('background')
+  if (isManifest3) {
+    views = views.filter(view => view !== 'background')
   }
 
   for (const view of views) {
     await fs.ensureDir(r(`extension/dist/${view}`))
     let data = await fs.readFile(r(`src/${view}/index.html`), 'utf-8')
     data = data
-      .replace('"./main.ts"', `"http://localhost:${port}/${view}/main.ts"`)
+      .replace('"./index.ts"', `"http://localhost:${port}/${view}/index.ts"`)
       .replace('<div id="app"></div>', '<div id="app">Vite server did not start</div>')
     await fs.writeFile(r(`extension/dist/${view}/index.html`), data, 'utf-8')
     log('PRE', `stub ${view}`)
